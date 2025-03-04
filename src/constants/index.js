c4571aa98b7773c312c4abb15476158075796ae8
PierreKoding <sitoruspierre@gmail.com>
2025-03-04 22:05:18 +0700

    Add Project

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,6 +2,7 @@ import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
+import project5 from "../assets/projects/project-5.jpg";
 
 export const HERO_CONTENT = `I am a third year student enrolled in the master track program at BINUS University. Please check out the rest of my portfolio to see my projects.`;
 
@@ -23,8 +24,16 @@ export const PROJECTS = [
     technologies: ["React", "NextJS", "CSS"],
   },
   {
-    title: "Household electricity consumption Machine Learning prediction",
+    title: "Floodster",
     image: project3,
+    link: "https://github.com/sheraldo-ux/WebProg-Blade",
+    description:
+      "A web-based flood mapping application that visualizes flood data and risk levels using Mapbox GL JS. It features real-time geolocation, interactive markers with white outlines for improved visibility, and a popup system that provides immediate feedback on flood risk based on user location.",
+    technologies: ["Laravel", "JS", "HTML", "Tailwind CSS", "Mapbox GL JS"],
+  },
+  {
+    title: "Household electricity consumption Machine Learning prediction",
+    image: project4,
     link: "https://github.com/PierreKoding/Machine_Learning_Household_Power_Consumption_Prediction",
     description:
       "A project where I compare which machine learning algorithm have the best accuracy to predict power consumption from a household and then uses tkinter to make the GUI so that the software is easier to use",
@@ -32,7 +41,7 @@ export const PROJECTS = [
   },
   {
     title: "Portfolio Website",
-    image: project4,
+    image: project5,
     link: "https://github.com/PierreKoding/Pierre-Portfolio",
     description:
       "A portfolio website showcasing my projects and skills as a developer.",
